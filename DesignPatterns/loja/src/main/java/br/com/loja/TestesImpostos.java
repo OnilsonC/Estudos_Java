fcc209880c9303baf45d87c96970dc807856d719
Onilson Cardoso <onilldta@yahoo.com.br>
2023-12-19 22:29:40 -0300

    DP - Strategy

--- DesignPatterns/loja/src/main/java/br/com/loja/TestesImpostos.java
+++ DesignPatterns/loja/src/main/java/br/com/loja/TestesImpostos.java
@@ -1,6 +1,8 @@
 package br.com.loja;
 
 import br.com.loja.imposto.CalculadoraDeImpostos;
+import br.com.loja.imposto.ICMS;
+import br.com.loja.imposto.ISS;
 import br.com.loja.imposto.TipoImposto;
 import br.com.loja.orcamento.Orcamento;
 
@@ -13,6 +15,6 @@ public class TestesImpostos {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
     }
 }
--- DesignPatterns/loja/src/main/java/br/com/loja/imposto/CalculadoraDeImpostos.java
+++ DesignPatterns/loja/src/main/java/br/com/loja/imposto/CalculadoraDeImpostos.java
@@ -6,14 +6,16 @@ import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
 
-    public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto) {
-        switch (tipoImposto) {
-            case ICMS:
-                return orcamento.getValor().multiply(new BigDecimal("0.1"));
-            case ISS:
-                return orcamento.getValor().multiply(new BigDecimal("0.06"));
-            default:
-                return BigDecimal.ZERO;
-        }
+    public BigDecimal calcular(Orcamento orcamento, Imposto tipoImposto) {
+        return tipoImposto.calcular(orcamento);
+
+//        switch (tipoImposto) {
+//            case ICMS:
+//                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+//            case ISS:
+//                return orcamento.getValor().multiply(new BigDecimal("0.06"));
+//            default:
+//                return BigDecimal.ZERO;
+//        }
     }
 }
