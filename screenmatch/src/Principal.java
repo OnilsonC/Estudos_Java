1f8cd88870d60caba87b517a9cc1821fdba94e75
Onilson Cardoso <onilldta@yahoo.com.br>
2024-01-29 21:40:07 -0300

    screenmatch/

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme filme1 = new Filme();
-        filme1.nome = "O Podesoso Chefão";
-        filme1.anoDeLancamento = 1972;
+        filme1.setNome("O Podesoso Chefão");
+        filme1.setAnoDeLancamento(1972);
 
         filme1.exibeFichaTecnica();
 
@@ -11,8 +13,8 @@ public class Principal {
         filme1.avalia(6);
         filme1.avalia(10);
 
-        System.out.println("Total das avaliações: " + filme1.totalDeAvaliacoes);
-        System.out.println("Soma das avaliações: " + filme1.somaDasAvaliacoes);
+        System.out.println("Total das avaliações: " + filme1.getTotalDeAvaliacoes());
+
         System.out.println("Média de avaliações: " + filme1.mediaAvaliacao());
     }
 }
