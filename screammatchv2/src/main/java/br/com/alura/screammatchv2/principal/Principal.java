ccfd862902f4c2340005d0fd835385a4f4123999
onilson-csf <onilson.cardoso@gmail.com>
2024-03-21 16:53:42 -0300

    initial commit

--- screammatchv2/src/main/java/br/com/alura/screammatchv2/principal/Principal.java
+++ screammatchv2/src/main/java/br/com/alura/screammatchv2/principal/Principal.java
@@ -3,9 +3,12 @@ package br.com.alura.screammatchv2.principal;
 import br.com.alura.screammatchv2.model.DadosEpisodio;
 import br.com.alura.screammatchv2.model.DadosSerie;
 import br.com.alura.screammatchv2.model.DadosTemporada;
+import br.com.alura.screammatchv2.model.Episodio;
 import br.com.alura.screammatchv2.service.ConsumoAPI;
 import br.com.alura.screammatchv2.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -24,7 +27,7 @@ public class Principal {
         var json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dadosSerie = converter.obterDados(json, DadosSerie.class);
 
-        System.out.println(dadosSerie);
+//        System.out.println(dadosSerie);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
@@ -34,7 +37,7 @@ public class Principal {
 
 			temporadas.add(dadosTemporada);
 		}
-		temporadas.forEach(System.out::println);
+//		temporadas.forEach(System.out::println);
 
 //        for (int i = 0; i < dadosSerie.totalTemporadas(); i++) {
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodio();
@@ -64,5 +67,28 @@ public class Principal {
                         .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
                         .limit(5)
                         .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodio().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de qual data pretende realizar a busca? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate buscaData = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(buscaData))
+                .forEach(e -> System.out.println(
+                        "Temporada " + e.getTemporada() + " [" +
+                                "Episodio " + e.getTitulo() +
+                                " Data lançamento " + e.getDataLancamento().format(df) + "]"
+                ));
+
     }
 }
